25540ea173aec3431012c4c09df0f98abac977b5
balasravatsal <vatsalbalasra24@gmail.com>
2023-06-06 14:49:52 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
-  
-}
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'about'} element={<About/>}/>
+                    <Route path={'products'} element={<Products/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
-
-export default App;
+export default App
